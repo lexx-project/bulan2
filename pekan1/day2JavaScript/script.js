462d5dd743834e659a90f30bb2abc2c570502a6e
lexx-project <passlexyganteng@gmail.com>
2025-09-02 13:27:47 +0700

    add task latihan mandiri and studi kasus from book 'Kitab Sakti Software Developer: Javascript Mastery'

--- pekan1/day2JavaScript/script.js
+++ pekan1/day2JavaScript/script.js
@@ -1,3 +1,5 @@
+// TUGAS MAS DANI
+
 // 1. Tampilkan biodata pakai console.log().
 console.log("Biodata:");
 console.log("Nama: John Doe");
@@ -24,3 +26,47 @@ if (hapus) {
 } else {
   console.log("Data batal dihapus.");
 }
+
+// TUGAS BUKU
+// LATIHAN MANDIRI
+// 1. Buat 3 variable dengan let const dan var
+let angka = 10;
+const namaa = "Budi";
+var isAktif = true;
+
+// 2.
+// Gunakan operator aritmatika dan perbandingan untuk membandingkan dua angka.
+let angka1 = 10;
+let angka2 = 7;
+let hasilTambah = angka1 + angka2;
+let hasilKali = angka1 * angka2;
+let perbandingan = angka1 > angka2;
+
+// 3. Tampilkan hasilnya dengan `console.log()`.
+console.log("Nilai angka:", angka);
+console.log("Nama:", namaa);
+console.log("Status aktif:", isAktif);
+console.log("Hasil tambah:", hasilTambah);
+console.log("Hasil kali:", hasilKali);
+console.log("Apakah angka1 > angka2?", perbandingan);
+console.log("Apakah angka1 === 10?", angka1 === 10);
+
+// STUDI KASUS
+// 1. Buat variabel nama produk, harga produk 1 dan 2.
+let namaProduk1 = "PANTENE";
+let namaProduk2 = "ZINK";
+let hargaProduk1 = 5000;
+ let hargaProduk2 = 4500;
+
+// 2. Hitung total harga dari dua produk.
+let totalHarga = hargaProduk1 + hargaProduk2;
+
+// 3. Tampilkan hasil total harga dengan teks seperti: `"Total harga untuk produk A dan B adalah RpXXX"`
+console.log(
+  "Total Harga Untuk " +
+    namaProduk1 +
+    " dan " +
+    namaProduk2 +
+    " adalah " +
+    totalHarga
+);
